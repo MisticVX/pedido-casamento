51bc5e0de01d4ec3a06ac5ac0b67fcfb334e519e
MisticVX <74386051+MisticVX@users.noreply.github.com>
2026-04-29 19:22:36 -0300

    Atualização corações

--- codigo.js
+++ codigo.js
@@ -163,19 +163,21 @@ sim.onclick = () => {
 
 // 💖 CORAÇÕES
 function iniciarCoracoes() {
-    setInterval(() => {
-        const heart = document.createElement("div");
-        heart.innerHTML = "💖";
-        heart.classList.add("heart");
+  setInterval(() => {
+    const heart = document.createElement("div");
+    heart.innerHTML = "💖";
+    heart.classList.add("heart");
 
-        heart.style.left = Math.random() * window.innerWidth + "px";
-        heart.style.fontSize = (Math.random() * 20 + 10) + "px";
-        heart.style.animationDuration = (Math.random() * 2 + 3) + "s";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.top = "0";
 
-        document.body.appendChild(heart);
+    heart.style.fontSize = (Math.random() * 20 + 10) + "px";
+    heart.style.animationDuration = (Math.random() * 2 + 3) + "s";
 
-        setTimeout(() => {
-            heart.remove();
-        }, 4000);
-    }, 300);
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+      heart.remove();
+    }, 4000);
+  }, 300);
 }
